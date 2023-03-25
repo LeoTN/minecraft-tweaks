754ef868a7695186d70159e129ae5e8428dd8e1d
Donnerbaer <37273909+LeoTN@users.noreply.github.com>
2023-03-25 08:35:38 +0100

    first launchable result

--- src/main/java/com/LeoTN/minecraft_tweaks/MinecraftTweaks.java
+++ src/main/java/com/LeoTN/minecraft_tweaks/MinecraftTweaks.java
@@ -1,15 +1,15 @@
-package main.java;
+package com.LeoTN.minecraft_tweaks;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MinecraftTeaks implements ModInitializer {
+public class MinecraftTweaks implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final String MOD_ID = "mctweaks";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MinecraftTeaks.MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MinecraftTweaks.MOD_ID);
 
     @Override
     public void onInitialize() {
@@ -17,6 +17,6 @@ public class MinecraftTeaks implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        LOGGER.info("Hello Fabric world!");
+        LOGGER.info("Minecraft Tweaks ready to load !");
     }
 }
